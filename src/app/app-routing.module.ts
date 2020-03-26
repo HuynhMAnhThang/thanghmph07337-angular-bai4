f964222a487831b21952b546a68738c447bcab10
HuynhManhThang <thanghm1234nam@gmail.com>
2020-03-26 17:31:44 +0700

    acbassdi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeCompComponent } from './homeComp/homeComp.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 const routes: Routes = [
  {path:'home',component:HomeCompComponent},
  {path:'',redirectTo:'home',pathMatch:'full'},
+ {path:'shopNow',component:ProductListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from'@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeCompComponent } from './homeComp/homeComp.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductServicesService } from './services/product-services.service';
+
 
 @NgModule({
    declarations: [
       AppComponent,
       HomeCompComponent,
+      ProductListComponent,
+
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule,
+      
    ],
-   providers: [],
+   providers: [ProductServicesService],
    bootstrap: [
       AppComponent
    ]
